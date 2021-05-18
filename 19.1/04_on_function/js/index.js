0ed11a12b27a4e1cea8ac87396128ed9df3a4ef5
alissar-arnaout <alissar@arnaout.com>
2021-05-17 20:38:08 -0400

    activities 19.1 + 19.2 added

---
+++ 19.1/04_on_function/js/index.js
@@ -0,0 +1,14 @@
+// Step 1: Hide a div
+$("#button1").on("click", function(){
+    $("hideMe").slideToggle();
+});
+
+// Step 2: Change a color
+$('#button2').on("click", function(){
+
+});
+
+// Step 3: Append content
+$('#button3').on("click", function(){
+
+});
