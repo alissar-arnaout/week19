0a3df88756b773047eaabeb99beb746c7ca93c08
alissar-arnaout <alissar@arnaout.com>
2021-05-19 21:02:03 -0400

    19.3 activites

---
+++ 19.3/Band_Animated/js/index.js
@@ -0,0 +1,28 @@
+// fade
+function fade() {
+    $('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 1000);
+
+// search bar
+$(".searchButton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+
+    if ($(".searchButton").hasClass("active")) {
+        $(".searchBar").css("height", "100vh");
+        $("#searchForm").css("opacity", "1");
+        $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    } 
+    else {
+        $(".searchBar").css("height", "0vh");
+        $("#searchForm").css("opacity", "0");
+        (".searchIcon").removeClass("fa-search").addClass("fa-search");
+    }
+});
+
+// accordian
+$(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+
+});
